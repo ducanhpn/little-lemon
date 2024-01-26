bc4e604a951f2199aff5ac811ecb745db28a1fb0
ducanhpn <phamngocducanh19@gmail.com>
2024-01-26 12:21:40 +0100

    Save Card.js

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@
 
 export default function Card({image, alt, description, name, price}){
     return <div className="card">
-        <img src={image} alt={alt} width="200" height="220"></img>
+        <img src={image} alt={alt}></img>
         <div className="flex-container">
             <h4>{name}</h4>
             <h4>{price}</h4>
